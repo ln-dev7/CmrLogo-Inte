e6030645ccf822b69379c0fc979375978679c427
ln-dev7 <leonelngoya@gmail.com>
2022-07-25 22:38:36 +0100

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,33 @@
+const overlay = document.querySelector(".overlay");
+const cardUpload = document.querySelector(".card-upload");
+const cardDownload = document.querySelector(".card-download");
 // ----------------------------------------------------------------------------
-const loader = document.querySelector("#loader");
-window.addEventListener("load", () => {
-  loader.classList.add("loaded");
-});
-// ----------------------------------------------------------------------------
-const menuClose = document.querySelector(".menu-close");
-const menuOpen = document.querySelector(".header-left-menu");
-const menu = document.querySelector(".menu");
+const btnUpload = document.querySelector(".header-right upload");
+btnUpload.addEventListener("click", () => {
+  overlay.classList.add("overlay-active");
+  cardDownload.classList.add("hide");
 
-menuOpen.addEventListener("click", () => {
-  menu.classList.add("menu-open");
+  
 });
-menuClose.addEventListener("click", () => {
-  menu.classList.remove("menu-open");
+// ----------------------------------------------------------------------------
+const logos = document.querySelectorAll(".logo");
+logos.forEach((logo) => {
+  logo.addEventListener("click", () => {
+    overlay.classList.add("overlay-active");
+    cardUpload.classList.add("hide");
+
+
+  });
 });
+// ----------------------------------------------------------------------------
+// const menuClose = document.querySelector(".menu-close");
+// const menuOpen = document.querySelector(".header-left-menu");
+// const menu = document.querySelector(".menu");
+
+// menuOpen.addEventListener("click", () => {
+//   menu.classList.add("menu-open");
+// });
+// menuClose.addEventListener("click", () => {
+//   menu.classList.remove("menu-open");
+// });
 // ----------------------------------------------------------------------------
